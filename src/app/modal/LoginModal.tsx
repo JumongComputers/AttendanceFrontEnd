85c68a97365660d08b31d10215ce19e2f428aa31
JumongComputers <jaytoluwase@gmail.com>
2024-03-17 16:21:15 +0100

    feat: Made login modal

--- src/app/modal/LoginModal.tsx
+++ src/app/modal/LoginModal.tsx
@@ -1,11 +1,17 @@
-'use client'
+// 'use client'
 import React, {useState} from "react"
 import { X } from "lucide-react";
 import Modal from "react-modal";
 import { useRouter } from "next/navigation";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { logInSchema } from "../utils/yupValidation";
+import {useSelector, useDispatch} from "react-redux";
+import { signIn } from "../redux/slices/loginSlice";
+import { SigninType } from "../utils/types";
+import Link from "next/link";
+// import { string } from "yup";
 
+// import { Provider } from "react-redux";
 
 interface LoginModalProps{
     isOpen: boolean,
@@ -17,9 +23,10 @@ interface LoginModalProps{
 
 const LoginModal: React.FC<LoginModalProps> = ({isOpen, onRequestClose}) =>{
 
+  const isLoading = useSelector((state: { auth: { loading: string } }) => state.auth.loading);
+  const dispatch = useDispatch();
 
-
-const router =useRouter();
+// const router =useRouter();
 
 if (!isOpen) return null
 
@@ -32,10 +39,10 @@ return(
             isOpen={isOpen}
             onRequestClose={onRequestClose}
             contentLabel="Login Modal"
-            className="absolute top-1/2 left-1/2 z-20 transform overflow-y-auto -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-4 w-full h-[60vh] lg:h-[80vh] max-w-xl md:max-w-4xl"
+            className="absolute top-1/2 left-1/2 z-20 transform overflow-y-none -translate-x-1/2 -translate-y-1/2 rounded-lg bg-transparent p-4 w-5/6 h-[80vh] lg:h-[75vh] md:max-w-4xl md:w-2/3 lg:w-1/2"
             overlayClassName="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center"
             >
-        
+             
             <Formik
             initialValues={{
                 email: "",
@@ -44,13 +51,13 @@ return(
             validationSchema={logInSchema}
             onSubmit={(values: any, {resetForm}) =>{
             console.log("Form Submitted with values:", values);
-            // dispatch()
+            dispatch(signIn(values as SigninType) as any);
             resetForm();
             
             }}
             >
                 <Form>
-                <div id="container" onClick={handleClose} className="fixed inset-0 flex items-center bg-black bg-opacity-50 justify-center z-20">
+                <div id="container" onClick={handleClose} className="fixed inset-0 flex items-center bg-black bg-opacity-100 justify-center z-20">
               <div className="bg-white w-full max-w-4xl rounded-md shadow-lg overflow-y-auto">
                 <div className="flex justify-end pt-4 pr-4">
                   <button onClick={() => onRequestClose()}>
@@ -60,10 +67,10 @@ return(
                 <div className="overflow-y-auto max-h-[520px]">
                   <div className="flex flex-col px-6 items-start w-full">
                     <div className="flex flex-col items-start lg:place-items-start w-full md:py-8 ">
-                      <span className="text-[#19202C] text-4xl font-bold">Login Page</span>
+                      <span className="text-black text-4xl font-bold mx-auto ">Login Page</span>
                       <div className="w-full grid gap-4 lg:gap-6 mb-6 mt-12 ">
                         <div className="flex flex-col">
-                          <label htmlFor="email" className="text-[#19202C] text-2xl mb-2">
+                          <label htmlFor="email" className="text-black text-2xl mb-2">
                             Email:
                           </label>
                           <Field
@@ -71,7 +78,7 @@ return(
                             type="text"
                             className="py-4 px-6 rounded-md bg-[#F2F7FF] focus:outline-none w-full
                               placeholder-gray-200::placeholder placeholder-opacity-75
-                              border-2 focus:border-[#0D60D8] text-xl"
+                              border-2 focus:border-black text-xl"
                             placeholder="Enter your Email"
                           />
                           <ErrorMessage name="email" component="div" className="text-red-600" />
@@ -85,22 +92,22 @@ return(
                             type="text"
                             className="py-4 px-6 rounded-md bg-[#F2F7FF] focus:outline-none w-full
                               placeholder-gray-200::placeholder placeholder-opacity-75
-                              border-2 focus:border-[#0D60D8] text-xl"
+                              border-2 focus:border-black text-xl"
                             placeholder="Enter your Password"
                           />
                           <ErrorMessage name="password" component="div" className="text-red-600" />
                         </div>
-                       
+                       <p><Link href='#' className="text-blue-500 hover:text-red-500">Click here to register</Link> if you dont have an account</p>
                         <div className="flex justify-between items-center pt-8">
                           <button
                             onClick={() => onRequestClose()}
-                            className="border border-[#0D60D8] py-4 text-[#0D60D8] rounded-md
-                              font-bold text-2xl focus:outline-none px-12 bg-white"
+                            className="bg-transparent border border-black  py-2 text-black rounded-md
+                              font-bold text-2xl focus:outline-none px-4 md:px-8 lg:px-10"
                           >
                             Cancel
                           </button>
-                          <button type="submit" className="bg-[#0D60D8] py-4 text-white rounded-md font-bold text-2xl px-6 focus:outline-none">
-                            {/* {isLoading === "pending" ? "Loading..." : "Book Restaurant"} */}
+                          <button type="submit" className="bg-black border border-transparent py-2 text-white rounded-md font-bold text-2xl px-4 focus:outline-none md:px-8 lg:px-10">
+                            {isLoading === "pending" ? "Loading..." : "Login"}
                           </button>
                         </div>
                       </div>
@@ -127,4 +134,4 @@ return(
 }
 
 
-export default LoginModal
+export default LoginModal;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
-import { motion } from "framer-motion";
-import Link from "next/link";
+"use client"
+import { useState } from "react";
+import LoginModal from "./modal/LoginModal";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
+// Provider
 
-export default function Home() {
+interface HomeProps {
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+const Home: React.FC<HomeProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   return (
     <main className="md:py-20 lg:py-40">
       <div className="w-7/6 flex flex-col justify-center align-middle text-center my-20 mx-auto border-3 border-white lg: ">
         <h1 className="text-white text-2xl font-bold md:text-6xl lg:text-8xl">Welcome to Attendy</h1>
         <span className="text-white md:text-2xl lg:text-3xl my-6">Your reliable Attendance App</span>
-        
-          <Link href="#">
-           <button className="bg-white mx-auto my-5 px-5 py-2">Click here to Login</button>
-          </Link>
+        <button onClick={openModal} className="bg-white mx-auto my-5 px-5 py-2">Click here to Login</button>
+        < Provider store={store}><LoginModal isOpen={isOpen} onRequestClose={closeModal}/></ Provider>
         
       </div>
     </main>
   );
 }
+
+export default Home;
